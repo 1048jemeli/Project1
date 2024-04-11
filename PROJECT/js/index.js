8422e12c4bbf7c663c9c9e605443306237bda620
Nazlin <jemelinazlin@gmail.com>
2024-04-11 20:06:04 +0300

    added js

---
+++ PROJECT/js/index.js
@@ -0,0 +1,60 @@
+
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const url = 'http://localhost:3000/restaurants';
+    fetchRestaurants(url);
+});
+
+function fetchRestaurants(url) {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data); 
+            data.forEach(restaurant => {
+                displayRestaurant(restaurant);
+            });
+        })
+        .catch(error => {
+            console.error("Error fetching restaurants:", error);
+        });
+}
+
+        
+        
+
+
+function displayRestaurant(restaurant) {
+    const listHolder = document.getElementById('restaurants');
+
+    const li = document.createElement('li');
+    li.className = 'restaurant-item';
+    li.style.cursor = "pointer";
+    li.textContent = restaurant.title.toUpperCase();
+    listHolder.appendChild(li);
+
+    li.addEventListener('click', () => {
+        setUpRestaurantDetails(restaurant);
+    });
+}
+
+function setUpRestaurantDetails(restaurant) {
+    const preview = document.getElementById('picture');
+    
+    preview.src = restaurant.picture;
+
+    const restaurantTitle = document.querySelector('#title');
+    restaurantTitle.textContent = restaurant.title;
+    
+    const restaurantLocation = document.querySelector('#location');
+    restaurantLocation.textContent = restaurant.location;
+    
+    const restaurantCuisineType = document.querySelector('#cuisine-type');
+    restaurantCuisineType.textContent = restaurant['Cuisine type'];
+
+    const restaurantOpeningHours = document.querySelector('#open');
+    restaurantOpeningHours.textContent = restaurant['open'];
+
+}
+
+
+
