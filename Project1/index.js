e613974d5ad2d5e7e2294f08d26eafe8b98677a1
Nazlin <jemelinazlin@gmail.com>
2024-04-12 18:11:21 +0300

    a fetch function with url from local file

--- Project1/index.js
+++ Project1/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   const url = "http://localhost:3001/restaurants";
-})
+  fetchRestaurants(url);
+});
+
+function fetchRestaurants(url) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      data.forEach((restaurant) => {
+        displayRestaurant(restaurant);
+      });
+    })
+    .catch((error) => {
+      console.error("Error fetching restaurants:", error);
+    });
+}
+
